a56aafc95379f04e783850bc3fdc4e29840d4997
zvryn <sarinkanok@gmail.com>
2024-05-29 09:55:54 +0700

    feat:add completed assignment conditional logic

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,14 @@
-//Exercise 1
+// Exercise #1: Light Bulb
 let lightBulbStatus = "On";
 
 // Start coding here.
+let message;
+if(lightBulbStatus ==="On"){
+    message="Light bulb is On.";
+}
+else{
+    message="Light bulb is Off.";
+};
+
+console.log(message);
+
